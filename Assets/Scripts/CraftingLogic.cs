04740b4db67226d8cd4a29b5c14e9d67a0515d47
Knrc <kontakt.mikey@gmail.com>
2024-05-14 19:53:37 +0200

    Started with Crafting System

--- Assets/Scripts/CraftingLogic.cs
+++ Assets/Scripts/CraftingLogic.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CraftingLogic : MonoBehaviour
 {
-    private int body;
-    private int magazin;
-    private int lauf;
-    private int schaft;
-
+    [Header("Crafting")] 
+    [SerializeField] private int _bodyPart = 0;
+    //1 Drill
+    //2 Hairdryer
+    //3 Banana
+    [SerializeField] private int magazin = 0;
+    //1 Toaster
+    //2 Can
+    //3 Battery
+    [SerializeField] private int barrel = 0;
+    //1 Raygun
+    //2 Gatlin
+    //3 Sniper barrel
+    [SerializeField] private int stock = 0;
+    //1 Spatula
+    //2 Shovel
+    //3 Spring
+    
     private string recipe;
 
     public void crafting()
     {
-        //Todo: Finish feature
+        //All possible Crafting recipe
+        //1,1,1,1 
+        //2,1,1,1
+        //3,1,1,1
+        //3,2,1,1
+        
     }
 }
