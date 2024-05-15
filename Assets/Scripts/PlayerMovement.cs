2816d435b567d51bd909a6b7c3167fb57a5f8297
Luk3Schro3d3r <126423225+Luk3Schro3d3r@users.noreply.github.com>
2024-05-15 13:15:10 +0200

    Character gets bigger if close and small if away

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float runSpeed = 20f;
+    public float sizeMultiplier;
+    private Vector2 _lastVelocity;
     private float _playerMovementDirection = 0;
     private Rigidbody2D _rigidbody2D;
     private PlayerControls _inputAction;
@@ -23,11 +25,24 @@ public class PlayerMovement : MonoBehaviour
         {
             _playerMovementDirection = moving.ReadValue<float>();
         };
+        _lastVelocity = _rigidbody2D.velocity;
     }
 
     private void FixedUpdate()
     {
         _rigidbody2D.velocity = _movementInput * runSpeed;
+
+        if (_rigidbody2D.velocity != _lastVelocity)
+        {
+            if (_rigidbody2D.velocity.y < 0 && transform.localScale.y < 3.0f)
+            {
+                transform.localScale *= sizeMultiplier;
+            }
+            else if (_rigidbody2D.velocity.y > 0 && transform.localScale.y > 1.0f)
+            {
+                transform.localScale /= sizeMultiplier;
+            }
+        }
     }
 
     private void OnMove(InputValue inputValue)
